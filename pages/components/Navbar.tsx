0fe10876a0fb37e6c83d9e03633e279fe1df8bc9
abhinav-suggaa <abhinav@suggaa.in>
2023-06-10 17:45:25 +0530

    added some more sty;es

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -1,30 +1,36 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className='bg-gray-800'>
-      <nav className='container mx-auto px-4 py-2'>
-        <ul className='flex justify-between items-center'>
-          <li className='mr-4'>
+    <div className="bg-gray-800">
+      <nav className="container mx-auto px-4 py-2">
+        <ul className="flex flex-col justify-center items-center space-y-4">
+          <li>
             <Link href="/interns">
-              <p className='text-white hover:text-gray-300'>Interns Details</p>
+              <p className="bg-gray-900 hover:bg-gray-700 text-white py-2 px-4 rounded-full text-center w-40">
+                Interns Details
+              </p>
             </Link>
           </li>
-          <li className='mr-4'>
+          <li>
             <Link href="/colleges">
-              <p className='text-white hover:text-gray-300'>College Details</p>
+              <p className="bg-gray-900 hover:bg-gray-700 text-white py-2 px-4 rounded-full text-center w-40">
+                College Details
+              </p>
             </Link>
           </li>
           <li>
             <Link href="/branches">
-              <p className='text-white hover:text-gray-300'>Branches Details</p>
+              <p className="bg-gray-900 hover:bg-gray-700 text-white py-2 px-4 rounded-full text-center w-40">
+                Branches Details
+              </p>
             </Link>
           </li>
         </ul>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
