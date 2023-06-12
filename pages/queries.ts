e9f54a8affaa0b736d8ce8301774d8ef11c91ac7
abhinav-suggaa <abhinav@suggaa.in>
2023-06-12 13:13:23 +0530

    added mutations to update in query.ts

--- pages/queries.ts
+++ pages/queries.ts
@@ -55,3 +55,28 @@ mutation DeleteBranch {
   }
 }
 `;
+
+export const UPDATE_INTERN = gql `
+mutation UpdateIntern {
+  update_intern_db_intern_by_pk({id : $id}, _set: {first_name: $first_name, last_name: $last_name, stipend: $stipend}) {
+    id
+    }
+    }
+    `;
+
+export const UPDATE_COLLEGE = gql `
+mutation UpdateCollege {
+  update_intern_db_college_by_pk({id : $id}, _set: {college_name: $college_name, college_address: $college_address}) {
+    id
+    }
+    }
+    `;
+
+export const UPDATE_BRANCH = gql `
+mutation UpdateBranch {
+  update_intern_db_branch_by_pk({id : $id}, _set: {branch_name: $branch_name, hod_name: $hod_name, hod_contact: $hod_contact}) {
+    id
+    }
+    }
+    `;
+    
